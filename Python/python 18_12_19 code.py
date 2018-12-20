5ffaebf038435eba89e9a8ea09182bb0bdcfd22c
Jeong-Ho Lee <37128105+littlehippo147@users.noreply.github.com>
2018-12-20 12:00:43 +0900

    Add files via upload

--- Python/python 18_12_19 code.py
+++ Python/python 18_12_19 code.py
@@ -3,6 +3,7 @@
 import tensorflow as tf
 import numpy as np
 import matplotlib.pyplot as plt
+import random
 from tensorflow.examples.tutorials.mnist import input_data
 
 tf.set_random_seed(777)
@@ -20,7 +21,7 @@ Y = tf.placeholder(tf.float32, shape = [None, 10])
 
 # Layer 1, X_img : (1, 28, 28, 1), Filter : (3, 3, 1, 32), stride : (1, 1), padding : 'SAME'
 # out_img: (?, 14, 14, 32)
-W1 = tf.Variable(tf.random_normal([3, 3, 1, 32]), name = 'weight1')
+W1 = tf.Variable(tf.random_normal([3, 3, 1, 32], stddev = 0.001), name = 'weight1')
 L1 = tf.nn.conv2d(X_img, W1, strides = [1, 1, 1, 1], padding = 'SAME')
 # conv L1 : (?, 28, 28, 32)
 # (N-F) / stride + 1 = (29-2) / 1 + 1 = 28
@@ -32,7 +33,7 @@ L1 = tf.nn.dropout(L1, keep_prob = keep_prob)
 # (N-F) / stride + 1 : (29-2) / 2 + 1 = 14
 
 # Layer 2, X_img : (?, 14, 14, 32), Filter : (3, 3, 32, 64), stride : (1, 1), padding : 'SAME'
-W2 = tf.Variable(tf.random_normal([3, 3, 32, 64]), name = 'weight2')
+W2 = tf.Variable(tf.random_normal([3, 3, 32, 64], stddev = 0.001), name = 'weight2')
 L2 = tf.nn.conv2d(L1, W2, strides = [1, 1, 1, 1], padding = 'SAME')
 # conv L2 : (?, 14, 14, 64)
 # (N-F) / stride + 1 = (15-2) / 1 + 1 = 14
@@ -46,7 +47,7 @@ L2 = tf.nn.dropout(L2, keep_prob = keep_prob)
 
 
 # Layer 3, X_img : (?, 7, 7, 64), Filter : (3, 3, 64, 128), stride : (1, 1), padding : 'SAME'
-W3 = tf.Variable(tf.random_normal([3, 3, 64, 128]), name = 'weight3')
+W3 = tf.Variable(tf.random_normal([3, 3, 64, 128], stddev = 0.001), name = 'weight3')
 L3 = tf.nn.conv2d(L2, W3, strides = [1, 1, 1, 1], padding = 'SAME')
 # conv L3 : (?, 7, , 64)
 # (N-F) / stride + 1 = (15-2) / 1 + 1 = 14
@@ -71,7 +72,7 @@ model = tf.matmul(L4, W5) + b5
 hypothesis = tf.nn.softmax(model)
 
 cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(labels = Y, logits = model))
-optimizer = tf.train.AdamOptimizer(learning_rate = 0.01)
+optimizer = tf.train.AdamOptimizer(learning_rate = learning_rate)
 train = optimizer.minimize(cost)
 
 sess = tf.Session()
@@ -94,13 +95,13 @@ for epoch in range(training_epoch):
 predict = tf.argmax(hypothesis, 1)
 accuracy = tf.reduce_mean(tf.cast(tf.equal(predict, tf.argmax(Y, 1)), dtype = tf.float32))
 
-a = sess.run(accuracy, feed_dict = {X : mnist.test.images, Y : mnist.test.labels, keep_prob : 0.7})
+a = sess.run(accuracy, feed_dict = {X : mnist.test.images, Y : mnist.test.labels, keep_prob : 1.0})
 print('accuracy :', a)
 
 # 랜덤 1개 값 예측
 r = random.randint(0, mnist.test.num_examples - 1)
 print('random :', r, 'Label :', sess.run(tf.argmax(mnist.test.labels[r:r+1],1)))
-print('Prediction :', sess.run(predict, feed_dict = {X : mnist.test.images[r:r+1], keep_prob : 1}))
+print('Prediction :', sess.run(predict, feed_dict = {X : mnist.test.images[r:r+1], keep_prob : 1.0}))
 
 # matplotlib : imshow()
 plt.imshow(mnist.test.images[r:r+1].reshape(28, 28), cmap = 'Greys', interpolation = 'nearest') # 흑백 2차원 보간법
